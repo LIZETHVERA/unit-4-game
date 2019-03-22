71bbe6886fbfe53ed224f0f4f9aeb24c8807c147
LIZETHVERA <lizeth.veraro@gmail.com>
2019-03-21 23:55:21 -0700

    Last commit game workin poor css'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,40 @@
-    var gameOn = false;
-    var chooseChar = false;
     var chooseAttacker = true;
     var chooseDefender = true;
-    var enemiesLeft = false;
-    var userDeath = false;
-    var computerDeath = false;
+    var enemiesLeft;
     var defender;
     var attacker;
     var winner;
-    var userDied = [];
     var counter = 0;
     var counterdefender = 0;
+    var wins = 0;
 
 var characters = {
    char1: {
-        name: 'obi',
+        name: 'Obi',
         healthPower: 120,
-        attackPower: 8,
-        counter: 6,
+        attackPower: 7,
         imageUrl: "assets/images/obi.jpg",
     },
     char2: {
-        name: "luke",
-        healthPower: 100,
-        attackPower: 5,
-        counter: 8,
+        name: "Luke",
+        healthPower: 140,
+        attackPower: 6,
         imageUrl: "assets/images/luke.jpg",
     },
     char3: {
-        name: "sidious",
-        healthPower: 100,
-        attackPower: 3,
-        counter: 8,
+        name: "Sidious",
+        healthPower: 130,
+        attackPower: 5,
         imageUrl: "assets/images/sidious.jpg",
     },
     char4: {
-        name: "maul",
-        healthPower: 100,
+        name: "Maul",
+        healthPower: 90,
         attackPower: 9,
-        counter: 8,
         imageUrl: "assets/images/maul.jpg",
     }
 };
+
 // the i in the fuction is index and the character is the value.
 $.each(characters, function(index,character) {
     var charDiv = $("<div class='character'>");
@@ -51,64 +44,62 @@ $.each(characters, function(index,character) {
     charDiv.data('name', character.name);
     charDiv.data('healthPower', character.healthPower);
     charDiv.data('attackPower', character.attackPower);
-    charDiv.data('counter', character.counter);
+    charDiv.data('counter', character.counter);{}
 
-    var charName = $("<div class='character-name'>").text(character.name);
+    var charName = $("<div class='character-name'>").text("Fighter: " + character.name);
     var charImage = $("<img alt='image' class='character-image'>").attr("src", character.imageUrl);
-    var charHealth = $("<div class='character-healthPower'>").text(character.healthPower);
+    var charHealth = $("<div class='character-healthPower'>").text("Health: " + character.healthPower);
 
-    //var charAttackPower = $("<div class='character-attack' data-attackPower='" + character.attackPower + "'>");
-    //var charCounter = $("<div class='character-counter' data-counter='" + character.counter + "'>");
-
-    // Append image and name divs
+     // Append image and name divs
     charDiv.append(charName).append(charImage).append(charHealth);
     charDiv.appendTo('#characters-section');
 });
 
-$(".character").on("click", function() {
-	//check for startgame conditions
-	if (!(gameOn) && !(chooseChar)) {
-        // flags that user needs to choose opponent next
-        chooseChar = true;
-        
-        // $(this).css("border-color", "#320B68");
 
+$(".character").on("click", function() {
+
+	//check for startgame conditions
+	if (chooseAttacker) {
+        // flags that user needs to choose opponent next
+        chooseAttacker = false;
+        
         // removes user from available
-        attacker = $(this).detach(); 
+        attacker = $(this).detach();
 
         // adds user to fight
         attacker.appendTo("#selected-character");
+     
+        attacker.off("click");
 
         //move the enemies left to the enemies available to attack. 
         $("#characters-section").appendTo("#enemies_left");
 
-    } else if (chooseChar) { //user is alive and needs a new opponent ** chooseDefender
-        
-        //flags functionality of fight and reset buttons, disables characters ** chooseDefender
-        chooseChar = false;
-        
-        gameOn = true; 
-        $("#alerts").html(""); // clears any alerts, if any
-        
-        // $(this).css("border-color", "#B9161A");
-        // Removes opponent from available
-		defender = $(this).detach();
-        
-        defender.appendTo("#defender");
-        
-        $("#characters-section").appendTo("#enemies_left"); 
-        
-        $(".character").css("background-color", "white");
-        
-        //move the enemies left to the enemies available to attack. 
-        $(".character","#enemies_left").css("background-color", "red");
-        
-console.log(defender);
-    }
+        } else if (chooseDefender) { //user is alive and needs a new opponent ** chooseDefender
+                
+            //flags functionality of fight and reset buttons, disables characters ** chooseDefender
+            
+            chooseDefender = false;
+                    
+            // gameOn = true; 
+            $("#gameMessage").html(""); // clears any alerts, if any
+            
+            // $(this).css("border-color", "#B9161A");
+            // Removes opponent from available
+            defender = $(this).detach();
+            
+            defender.appendTo("#defender");
+            
+            enemiesLeft = $("#characters-section").appendTo("#enemies_left"); 
+            console.log("enemies left" + enemiesLeft);
+            $(".character").css("background-color", "white");
+            
+            //move the enemies left to the enemies available to attack. 
+            $(".character","#enemies_left").css("background-color", "red");
+            
+        }
 });
 
 function fight (){
-
     counter ++;
     counterdefender ++;
     console.log(counter);
@@ -118,14 +109,13 @@ function fight (){
 
     var healthPower = defender.data('healthPower');
     var defenderHealth = parseInt(healthPower);
-    console.log("defender healt1" + defenderHealth);
+  
 
     // To get the attacker heatlh value.
     var healthPowerAttacker = attacker.data('healthPower');
     var attackerHealth = parseInt(healthPowerAttacker);
-    console.log("attacker healt1" + attackerHealth);
     
-    
+        
     // Decrese the defender power.
     defenderHealth-= attackerAttack; 
  
@@ -134,21 +124,20 @@ function fight (){
 
    // defender attack power. 
     var defenderAttack = parseInt(defender.data("attackPower"));
-    console.log("defender attack: " + defenderAttack);
+   
     attackerHealth -= defenderAttack;
     var damage = defenderAttack * counterdefender;
-    console.log("attacker healh" +  attackerHealth);
+  
     //change data on user attack 
 
-    
     // to find the healthPower character 
      $("#defender").find(".character-healthPower").html(defenderHealth);
      $("#selected-character").find(".character-healthPower").html(attackerHealth);
     console.log("attack to defender inicio" + attackerAttack);
     //messsge to the user
     $("#gameMessage").html('');
-    $("#gameMessage").append("You did " + attackerAttack + " damage to " + defender.data("name") + ". "); 
-    $("#gameMessage").append( defender.data("name") + "damage to you " + damage); 
+    $("#gameMessage").append("You did: " + attackerAttack + " damage to " + defender.data("name") + ". "); 
+    $("#gameMessage").append( defender.data("name") + "  damage to you by: " + damage); 
 
     // update attack power
      
@@ -158,29 +147,57 @@ function fight (){
     console.log("attack to defender final" + attackerAttack);
 
     if (defenderHealth <= 0){
-            $("#gameMessage").html('');
-            $("#gameMessage").append("You have defeated " + defender.data("name") + ". Choose a new character");
-               
-            defender.remove();
-            chooseChar = true; //** chooseDefender
-            counterdefender = 0;
-             
-    } else if (chooseChar) {
-        $("#gameMessage").text("please select a character");
+
+        $("#gameMessage").html('');
+        $("#gameMessage").append("You have defeated " + defender.data("name") + "  Your power attack now is: " + attackerAttack + " . Choose a new character");
+        
+        defender.remove();
+        chooseDefender = true; //** chooseDefender
+        counterdefender = 0;
+        wins++;
     
+        } else if (chooseAttacker) {
+              $("#gameMessage").text("Please select a character");
+    
+            }  else if (attackerHealth <= 0 ){
+                $("#gameMessage").html('');
+                $("#gameMessage").append("GAME OVER");
+                $("#attack-button").detach();
+                $("#defender").detach();
+                $("#enemies_left").detach();
+
+    }
+
+    if (wins == Object.keys(characters).length - 1){
+        $("#gameMessage").html('');
+        $("#gameMessage").append("You Win!");
+        $("#attack-button").detach();
+        $("#defender").detach();
+        $("#enemies_left").detach();
     }
 
 }
  
-
 $("#attack-button").on("click", function() {
-    if (chooseChar) {
-        $("#gameMessage").text("please select a character");
+    if (!chooseDefender && !chooseAttacker) {
         
+        fight();   
+
+    } else {
+
+        $("#gameMessage").text("please select a character");
+    
     }
-		fight();
+     
+})
+
+
+$("#restart-button").on("click", function() {
+    window.location.reload();
 
 })
+
+
 
 
 
